f126adcaea7a571761ce1052bbd276542175dcaf
Prashant Choudhary <prashant.srs@gmail.com>
2019-04-11 21:18:15 +0530

    variable name change

--- src/index.js
+++ src/index.js
@@ -6,18 +6,18 @@ import Step from "./Step";
 import "./styles.css";
 
 function App() {
-  const [screen, setScreen] = useState(1);
+  const [currentstep, setCurrentStep] = useState(1);
   return (
     <React.Fragment>
-      <Steps step={screen}>
+      <Steps step={currentstep}>
         <Step>First Screen</Step>
         <Step>Second Screen</Step>
         <Step>Third Screen</Step>
       </Steps>
       <div style={{ marginTop: "20px" }}>
-        <button onClick={() => setScreen(1)}>Show Screen 1</button>
-        <button onClick={() => setScreen(2)}>Show Screen 2</button>
-        <button onClick={() => setScreen(3)}>Show Screen 3</button>
+        <button onClick={() => setCurrentStep(1)}>Show Screen 1</button>
+        <button onClick={() => setCurrentStep(2)}>Show Screen 2</button>
+        <button onClick={() => setCurrentStep(3)}>Show Screen 3</button>
       </div>
     </React.Fragment>
   );
